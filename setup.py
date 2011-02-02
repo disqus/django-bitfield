fad46d80fdedf4dd503c70495d309fee81a2126b
David Cramer <dcramer@gmail.com>
2011-02-02 11:14:08 -0800

    Bump verison to 1.2.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ class mytest(test):
 
 setup(
     name='django-bitfield',
-    version='1.1.0',
+    version='1.2.0',
     author='DISQUS',
     author_email='opensource@disqus.com',
     url='http://github.com/disqus/django-bitfield',
