5fd25422ede34ca7726807b22e3c0d43f8bc32dc
Martino Pizzol <martino@ahref.eu>
2012-06-13 14:41:36 +0200

    Fixed BitFieldCheckboxSelectMultiple rendering with POST data(list of string)

--- bitfield/forms.py
+++ bitfield/forms.py
@@ -6,7 +6,7 @@ from .types import BitHandler
 
 class BitFieldCheckboxSelectMultiple(CheckboxSelectMultiple):
     def render(self, name, value, attrs=None, choices=()):
-        if value is not None:
+        if type(value) is BitHandler:
             value = [k for k, v in value if v]
         return super(BitFieldCheckboxSelectMultiple, self).render(
           name, value, attrs=attrs, choices=enumerate(choices))
