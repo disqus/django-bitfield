41ce10da0529876c5fd5c48c79b12a2faf73cb78
David Cramer <dcramer@gmail.com>
2012-10-01 09:31:54 -0700

    Keep a reasonable unknown choice error message

--- bitfield/forms.py
+++ bitfield/forms.py
@@ -42,5 +42,5 @@ class BitFormField(IntegerField):
             try:
                 setattr(result, str(k), True)
             except AttributeError:
-                raise ValidationError('Unknown choice')
+                raise ValidationError('Unknown choice: %r' % k)
         return int(result)
