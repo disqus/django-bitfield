481496a6bd0a62ae936413f0b117824971512bbc
David Cramer <dcramer@gmail.com>
2012-10-01 12:48:46 -0700

    More general cleanup

--- bitfield/forms.py
+++ bitfield/forms.py
@@ -24,23 +24,20 @@ class BitFieldCheckboxSelectMultiple(CheckboxSelectMultiple):
 
 
 class BitFormField(IntegerField):
-    """
-    'choices' should be a flat list of flags (just as BitField
-    accepts them).
-    """
     def __init__(self, choices=(), widget=BitFieldCheckboxSelectMultiple, *args, **kwargs):
         self.widget = widget
+        super(BitFormField, self).__init__(widget=widget, *args, **kwargs)
         self.choices = self.widget.choices = choices
-        super(BitFormField, self).__init__(*args, **kwargs)
 
     def clean(self, value):
         if not value:
             return 0
 
+        # Assume an iterable which contains an item per flag that's enabled
         result = BitHandler(0, [k for k, v in self.choices])
         for k in value:
             try:
                 setattr(result, str(k), True)
             except AttributeError:
-                raise ValidationError('Unknown choice: %r' % k)
+                raise ValidationError('Unknown choice: %r' % (k,))
         return int(result)
