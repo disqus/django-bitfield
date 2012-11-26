058a54f5709d2a8e9f3801fcce7811c91c4f2825
David Cramer <dcramer@gmail.com>
2012-11-26 13:08:45 -0800

    1.6.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-bitfield',
-    version='1.6.3',
+    version='1.6.4',
     author='DISQUS',
     author_email='opensource@disqus.com',
     url='http://github.com/disqus/django-bitfield',
