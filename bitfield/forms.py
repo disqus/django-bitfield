129d0f584f3f0a35ef829b8f706953d0dc86c89f
Alexander Atanasov <aatanasov@gmail.com>
2013-06-10 13:49:26 +0300

    Convert initial value that comes as int to a list of flag names.
    When editing an object the widget works as expected.
    The exception occurs only when adding a new object.

--- bitfield/forms.py
+++ bitfield/forms.py
@@ -25,6 +25,13 @@ class BitFieldCheckboxSelectMultiple(CheckboxSelectMultiple):
 
 class BitFormField(IntegerField):
     def __init__(self, choices=(), widget=BitFieldCheckboxSelectMultiple, *args, **kwargs):
+       if isinstance(kwargs['initial'],int):
+           iv = kwargs['initial']
+            l = []
+            for i in range(0, 63):
+               if (1<<i) & iv > 0:
+                       l += [choices[i][0]]
+            kwargs['initial'] = l
         self.widget = widget
         super(BitFormField, self).__init__(widget=widget, *args, **kwargs)
         self.choices = self.widget.choices = choices
