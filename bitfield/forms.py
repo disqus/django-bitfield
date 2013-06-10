7a6ca66c0e7925a1fe7c5a37740d23e3bf7d8bc1
Alexander Atanasov <aatanasov@gmail.com>
2013-06-10 22:30:11 +0300

    Style cleanup. Check if initial is in kwargs.

--- bitfield/forms.py
+++ bitfield/forms.py
@@ -25,12 +25,12 @@ class BitFieldCheckboxSelectMultiple(CheckboxSelectMultiple):
 
 class BitFormField(IntegerField):
     def __init__(self, choices=(), widget=BitFieldCheckboxSelectMultiple, *args, **kwargs):
-        if isinstance(kwargs['initial'],int):
-            iv = kwargs['initial']
+        iv = kwargs.get('initial')
+        if isinstance(iv, int):
             l = []
-            for i in range(0, 63):
-               if (1<<i) & iv > 0:
-                       l += [choices[i][0]]
+            for i in xrange(0, 63):
+                if (1 << i) & iv > 0:
+                    l += [choices[i][0]]
             kwargs['initial'] = l
         self.widget = widget
         super(BitFormField, self).__init__(widget=widget, *args, **kwargs)
