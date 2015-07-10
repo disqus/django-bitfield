413b06447c4b72defb3d7dccf7e18a946c539100
olleh <my.publick@gmail.com>
2015-07-10 16:39:17 +0900

    Use bitwise not/and instead of stupid xor.

--- bitfield/query.py
+++ bitfield/query.py
@@ -47,16 +47,9 @@ class BitQuerySaveWrapper(BitQueryLookupWrapper):
 
         This will be called by Where.as_sql()
         """
-        engine = connection.settings_dict['ENGINE'].rsplit('.', -1)[-1]
-        if engine.startswith('postgres'):
-            XOR_OPERATOR = '#'
-        elif engine.startswith('sqlite'):
-            raise NotImplementedError
-        else:
-            XOR_OPERATOR = '^'
 
         if self.bit:
             return ("%s.%s | %d" % (qn(self.table_alias), qn(self.column), self.bit.mask),
                     [])
-        return ("%s.%s %s %d" % (qn(self.table_alias), qn(self.column), XOR_OPERATOR, self.bit.mask),
+        return ("%s.%s & ~%d" % (qn(self.table_alias), qn(self.column), self.bit.mask),
                 [])
