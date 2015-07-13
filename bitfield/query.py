4b5561511699a89cb1f404907b30ab72764f83e7
olleh <my.publick@gmail.com>
2015-07-13 22:56:43 +0900

    Fixed an error where BitQueryLookupWrapper includes an unprocessed rhs value

--- bitfield/query.py
+++ bitfield/query.py
@@ -32,7 +32,7 @@ try:
                 lhs_sql = lhs_sql + ' & %s'
             else:
                 lhs_sql = lhs_sql + ' | %s'
-            params.extend(self.get_db_prep_lookup(self.rhs, connection)[1])
+            params.extend(self.process_rhs(qn, connection)[1])
             return lhs_sql, params
 
 except ImportError:
