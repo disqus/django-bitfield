2d196d0441c90e898435e1137cec095ac0ba6ac1
Mathieu Comandon <strycore@gmail.com>
2016-08-16 14:33:10 -0700

    Fix AppConfig name

--- bitfield/__init__.py
+++ bitfield/__init__.py
@@ -6,7 +6,7 @@ from __future__ import absolute_import
 
 from bitfield.models import Bit, BitHandler, CompositeBitField, BitField  # NOQA
 
-default_app_config = 'bitfield.apps.BitFieldConfig'
+default_app_config = 'bitfield.apps.BitFieldAppConfig'
 
 try:
     VERSION = __import__('pkg_resources') \
