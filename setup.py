f9cd4ce21fd7335ff7958e42ef1afefe4f2ed54d
Burak Yigit Kaya <byk@disqus.com>
2017-04-29 02:39:02 +0300

    1.9.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-bitfield',
-    version='1.9.2',
+    version='1.9.3',
     author='Disqus',
     author_email='opensource@disqus.com',
     url='https://github.com/disqus/django-bitfield',
