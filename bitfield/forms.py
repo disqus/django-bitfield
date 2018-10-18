e74420b90e83ade7956023eaf4ef2613e441a9ca
Tim Abbott <tabbott@zulipchat.com>
2018-10-18 14:10:33 -0700

    forms: Fix linter error with ambiguous variable name 'l'.
    This has been making CI fail for basically every new PR that's opened.

--- bitfield/forms.py
+++ bitfield/forms.py
@@ -43,11 +43,11 @@ class BitFormField(IntegerField):
 
         if isinstance(kwargs['initial'], int):
             iv = kwargs['initial']
-            l = []
+            iv_list = []
             for i in range(0, min(len(choices), 63)):
                 if (1 << i) & iv > 0:
-                    l += [choices[i][0]]
-            kwargs['initial'] = l
+                    iv_list += [choices[i][0]]
+            kwargs['initial'] = iv_list
         self.widget = widget
         super(BitFormField, self).__init__(widget=widget, *args, **kwargs)
         self.choices = self.widget.choices = choices
