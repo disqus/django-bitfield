893b5ae232de88c8368a41b40865459a9e1d11e5
Mateusz Mandera <mateusz.mandera@protonmail.com>
2019-09-03 09:07:02 +0200

    query: Eliminate old Django compatibility logic.
    Django >= 1.10 is now guaranteed, so the try/except pattern can be
    deleted and the block inside the "try" clause should always be run.

--- bitfield/query.py
+++ bitfield/query.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 from bitfield.types import Bit, BitHandler
+from django.db.models.lookups import Exact
 
 
 class BitQueryLookupWrapper(object):
@@ -23,30 +24,23 @@ class BitQueryLookupWrapper(object):
                 [])
 
 
-try:
-    # Django 1.7+
-    from django.db.models.lookups import Exact
+class BitQueryLookupWrapper(Exact):  # NOQA
+    def process_lhs(self, qn, connection, lhs=None):
+        lhs_sql, params = super(BitQueryLookupWrapper, self).process_lhs(
+            qn, connection, lhs)
+        if self.rhs:
+            lhs_sql = lhs_sql + ' & %s'
+        else:
+            lhs_sql = lhs_sql + ' | %s'
+        params.extend(self.get_db_prep_lookup(self.rhs, connection)[1])
+        return lhs_sql, params
 
-    class BitQueryLookupWrapper(Exact):  # NOQA
-        def process_lhs(self, qn, connection, lhs=None):
-            lhs_sql, params = super(BitQueryLookupWrapper, self).process_lhs(
-                qn, connection, lhs)
-            if self.rhs:
-                lhs_sql = lhs_sql + ' & %s'
-            else:
-                lhs_sql = lhs_sql + ' | %s'
-            params.extend(self.get_db_prep_lookup(self.rhs, connection)[1])
-            return lhs_sql, params
+    def get_db_prep_lookup(self, value, connection, prepared=False):
+        v = value.mask if isinstance(value, (BitHandler, Bit)) else value
+        return super(BitQueryLookupWrapper, self).get_db_prep_lookup(v, connection)
 
-        def get_db_prep_lookup(self, value, connection, prepared=False):
-            v = value.mask if isinstance(value, (BitHandler, Bit)) else value
-            return super(BitQueryLookupWrapper, self).get_db_prep_lookup(v, connection)
-
-        def get_prep_lookup(self):
-            return self.rhs
-
-except ImportError:
-    pass
+    def get_prep_lookup(self):
+        return self.rhs
 
 
 class BitQuerySaveWrapper(BitQueryLookupWrapper):
