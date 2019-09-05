491c1eb17b46cc16056ce122463bb22e9e10bb39
Mateusz Mandera <mateusz.mandera@protonmail.com>
2019-09-05 17:06:53 +0200

    query: Remove leftover class definition.
    This should have been deleted in the previous commit. This definition of
    BitQueryLookupWrapper gets immediately overwritten by the definition
    below, and obviously should be deleted.

--- bitfield/query.py
+++ bitfield/query.py
@@ -4,26 +4,6 @@ from bitfield.types import Bit, BitHandler
 from django.db.models.lookups import Exact
 
 
-class BitQueryLookupWrapper(object):
-    def __init__(self, alias, column, bit):
-        self.table_alias = alias
-        self.column = column
-        self.bit = bit
-
-    def as_sql(self, qn, connection=None):
-        """
-        Create the proper SQL fragment. This inserts something like
-        "(T0.flags & value) != 0".
-
-        This will be called by Where.as_sql()
-        """
-        if self.bit:
-            return ("(%s.%s | %d)" % (qn(self.table_alias), qn(self.column), self.bit.mask),
-                    [])
-        return ("(%s.%s & %d)" % (qn(self.table_alias), qn(self.column), self.bit.mask),
-                [])
-
-
 class BitQueryLookupWrapper(Exact):  # NOQA
     def process_lhs(self, qn, connection, lhs=None):
         lhs_sql, params = super(BitQueryLookupWrapper, self).process_lhs(
