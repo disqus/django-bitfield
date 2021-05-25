67d639c611c8379de9fa9d226ae10d7875ef4cb1
Tim Abbott <tabbott@zulip.com>
2021-05-25 09:13:00 -0700

    setup: Declare support up to Python 3.9.

--- setup.py
+++ setup.py
@@ -54,5 +54,9 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )
